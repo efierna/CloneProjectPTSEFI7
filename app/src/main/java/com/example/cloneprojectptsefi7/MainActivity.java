62322e645b548e9ec173f061187766512e52f577
efierna <efi2018rplrus2@gmail.com>
2020-10-07 12:34:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cloneprojectptsefi7/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cloneprojectptsefi7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
